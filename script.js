a1a64c54cf4fa924692be039e8c4f4d478a8a824
rynru <rynru@example.com>
2026-09-09 21:01:25 +0300

    update background and styles

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-document.addEventListener("mousemove", function(e) {
-    let glow = document.querySelector(".mouse-glow");
-    let bg = document.getElementById("bg");
+    document.addEventListener("mousemove", function(e) {
+        let glow = document.querySelector(".mouse-glow");
+        let bg = document.getElementById("bg");
 
-    if (glow) {
-        glow.style.left = e.clientX - 100 + "px";
-        glow.style.top = e.clientY - 100 + "px";
-    }
+        if (glow) {
+            glow.style.left = e.clientX - 100 + "px";
+            glow.style.top = e.clientY - 100 + "px";
+        }
 
-    if (bg) {
-        let width = window.innerWidth;
-        let height = window.innerHeight;
-        
-        let xPos = (e.clientX / width - 0.5) * 16;
-        let yPos = (e.clientY / height - 0.5) * 16;
+        if (bg) {
+            let width = window.innerWidth;
+            let height = window.innerHeight;
+            
+            let xPos = (e.clientX / width - 0.5) * 16;
+            let yPos = (e.clientY / height - 0.5) * 16;
 
-        bg.style.transform = `translate(${-xPos}px, ${-yPos}px)`;
-    }
-});
+            bg.style.transform = `translate(${-xPos}px, ${-yPos}px)`;
+        }
+    });
